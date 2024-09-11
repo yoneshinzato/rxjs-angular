73fe10dbd964fc309d3736f145f5e1e9fbfc532c
Yone <chibariyo@gmail.com>
2024-09-10 22:05:35 -0300

    stateless service

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,51 @@
+import {Component, OnInit} from '@angular/core';
+import {Course, sortCoursesBySeqNo} from '../model/course';
+import {interval, noop, Observable, of, throwError, timer} from 'rxjs';
+import {catchError, delay, delayWhen, filter, finalize, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
+import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
+import { CoursesService } from '../services/courses.service';
+
+
+@Component({
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  beginnerCourses: Course[];
+
+  advancedCourses: Course[];
+
+
+  constructor(private coursesService: CoursesService, private dialog: MatDialog) {
+
+  }
+
+  ngOnInit() {
+
+    
+
+  }
+
+  editCourse(course: Course) {
+
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = "400px";
+
+    dialogConfig.data = course;
+
+    const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
+
+  }
+
+}
+
+
+
+
---
+++ src/app/services/courses.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Course } from '../model/course';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesService {
+
+  constructor(private http: HttpClient) { }
+// service layer return only observables to the view layer. it is stateless, does not hold app data
+  loadAllCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>('/api/courses')
+      .pipe(
+        // map create a derived observable
+        map(res => res["payload"]),
+      )
+
+}
