1379006ede69e9a43e18c74fee31156e12627aa6
Yone <chibariyo@gmail.com>
2024-09-21 19:55:39 -0300

    implementation finished on app component

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Observable} from 'rxjs';
 import {Message} from '../model/message';
 import {tap} from 'rxjs/operators';
+import { MessagesService } from './messages.service';
 
 @Component({
   selector: 'messages',
@@ -11,12 +12,16 @@ import {tap} from 'rxjs/operators';
 export class MessagesComponent implements OnInit {
   showMessages = false;
 
-  constructor() {
+  errors$: Observable<string[]>;
 
+  constructor(public messasgesService: MessagesService) {
   }
 
   ngOnInit() {
-
+    this.errors$ = this.messasgesService.errors$
+      .pipe(
+        tap(() => this.showMessages = true)
+      )
 
   }
 
--- src/app/messages/messages.service.ts
+++ src/app/messages/messages.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Injectable()
 export class MessagesService {
 
+  // armazena ultimo valor emitido e emite o valor para novos assinantes
+  private subject = new BehaviorSubject<string[]>([]); 
+
+// expõe behaviorsubject como observable, permitindo que outros componentes se inscrevam pra receber atualizações sobre os erros
+  errors$: Observable<string[]> = this.subject.asObservable().pipe(
+    filter(messages => messages && messages.length > 0)
+  ); 
+
   constructor() { }
 
   showErrors(...errors: string[]) {
-    
+    this.subject.next(errors)
   }
 
 }
