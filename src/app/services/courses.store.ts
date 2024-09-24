148ea4d3a0d8d3436b7ee7c0d9578c1ed0714dce
Yone <chibariyo@gmail.com>
2024-09-23 23:48:15 -0300

    store optimistic data modifications step by step implementation

--- src/app/services/courses.store.ts
+++ src/app/services/courses.store.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, throwError } from 'rxjs';
 import { Course, sortCoursesBySeqNo } from '../model/course';
-import { catchError, map, tap } from 'rxjs/operators';
+import { catchError, map, shareReplay, tap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { MessagesService } from '../messages/messages.service';
 import { LoadingService } from '../loading/loading.service';
@@ -12,7 +12,7 @@ import { LoadingService } from '../loading/loading.service';
 
 
 export class CoursesStore {
-
+    // remember the last value emitted by courses$
     private subject = new BehaviorSubject<Course[]>([]);
     courses$: Observable<Course[]> = this.subject.asObservable();
 
@@ -34,7 +34,33 @@ export class CoursesStore {
     }
 
     saveCourse(courseId: string, changes: Partial<Course>): Observable<any> {
+        // update data in memory, in the client side, getValue - gets the last value emitted
+        const courses = this.subject.getValue();
+
+        const index = courses.findIndex(course => course.id == courseId);
+        // only exists in memory
+        const newCourse: Course = {
+            ...courses[index],
+            ...changes
+        }
+        // create a copy of the courses array above
+        const newCourses: Course[] = courses.slice(0)   
+
+        newCourses[index] = newCourse;
+
+        // trigger a save to the backend, application reflect these changes immediately
+        this.subject.next(newCourses);
+
         return this.http.put(`/api/courses/${courseId}`, changes)
+            .pipe(
+                catchError(err => {
+                    const message = "Could not save course";
+                    console.log(message, err);
+                    this.messages.showErrors(message);
+                    return throwError(err)
+                }),
+                shareReplay()
+            )
     }
 
     private loadAllCourses() {
