f4942161e56a4d102bd3c923583dd72acf6cd1b1
Yone <chibariyo@gmail.com>
2024-10-02 00:48:04 -0300

    auth store browser refresh support with loca storage

--- src/app/services/auth.store.ts
+++ src/app/services/auth.store.ts
@@ -4,6 +4,8 @@ import { User } from '../model/user';
 import { map, shareReplay, tap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 
+const AUTH_DATA = 'auth_data';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -19,18 +21,29 @@ export class AuthStore {
     this.isLoggedIn$ = this.user$.pipe(map(user => !!user)) // !!user means not null
 
     this.isLoggedOut$ = this.isLoggedIn$.pipe(map(loggedIn => !loggedIn))
+
+    const user = localStorage.getItem(AUTH_DATA) ;
+    
+    if(user) {
+      this.subject.next((JSON.parse(user)));
+    }
+
   }
   
   login(email: string, password: string): Observable<User> {
     return this.http.post<User>('/api/login', { email, password })
       .pipe(
-        tap(user => this.subject.next(user)), // save user in subject
+        tap(user => {
+          this.subject.next(user);
+          localStorage.setItem(AUTH_DATA, JSON.stringify(user)); // save user in local storage, as string
+        }), // save user in subject
         shareReplay()
       )
   }
 
   logout() {
     this.subject.next(null); // null means not logged in
+    localStorage.removeItem(AUTH_DATA);
   }
 
 }
